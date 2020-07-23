0d2209accae906d7e3eea242a571fda71b860d57
jdvolk <justin.d.volk@gmail.com>
2020-07-23 17:32:08 -0600

    cleaned code

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,16 +1,8 @@
 
 const url = "http://localhost:3002/api/v1/shoes";
 
-// fetch("localhost:3002/api/v1/shoes/", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
 export const getAllShoes = async () => {
-  
-  debugger
   const response = await fetch(url);
   const shoes = await response.json();
-  console.log(shoes)
   return shoes;
 }
