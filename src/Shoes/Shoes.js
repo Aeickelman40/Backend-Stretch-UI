0cc00d2152d65dde5fc7920c67bafc35abbc928d
jdvolk <justin.d.volk@gmail.com>
2020-07-25 10:18:15 -0600

    changed props passed into shoebox, displayed image in shoebox, some styling for the shoe box, add media query for mobile

--- src/Shoes/Shoes.js
+++ src/Shoes/Shoes.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-const Shoes = ({brand, colorway, release_date, retail_price, model }) => {
+const Shoes = ({...props }) => {
     return (
         <section>
-            <h1>Model: {model}</h1>
-            <p>Release Date: {release_date}</p>
-            <p>Color: {colorway}</p>
-            <p>Brand: {brand}</p>
-            <p>Cost: {retail_price}</p>
+            <h1>Model: {props.model}</h1>
+            <p>Release Date: {props.release_date}</p>
+            <p>Color: {props.colorway}</p>
+            <p>Brand: {props.brand}</p>
+            <p>Cost: {props.retail_price}</p>
+            <img
+                src={props.thumb_url}
+                alt={props.title}
+                className="sneaker-image"
+            />
         </section>
     )
 }
