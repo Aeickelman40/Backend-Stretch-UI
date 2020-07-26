d0a0050fc277f9694024bd2cd92dee021ada0031
Alex Eickelman <aeickelman40@gmail.com>
2020-07-26 12:40:21 -0600

    Add data for expanded shoe, remove data for each shoe at home page

--- src/ExpandedShoe/ExpandedShoe.js
+++ src/ExpandedShoe/ExpandedShoe.js
@@ -4,9 +4,46 @@ import React from "react"
 const ExpandedShoe = ({ ...props }) => {
     console.log(props)
     return (
-        <h1>{props.model}</h1>
+        <section>
+            <h1>Model: {props.model}</h1>
+            <p>Brand: {props.brand}</p>
+            <p>Color: {props.colorway}</p>
+            <p>Release Date: {props.release_date}</p>
+            <p>Demographic: {props.demographic}</p>
+            <p>Price: {props.retail_price}</p>
+            {/* <img
+                src={props.image_url}
+                alt={props.title}
+                className="sneaker-image"
+            />
+            <img
+                src={props.thumb_url}
+                alt={props.title}
+                className="sneaker-image"
+            /> */}
+            <img
+                src={props.small_image_url}
+                alt={props.title}
+                className="sneaker-image"
+            />
+        </section>
     )
 }
 
 export default ExpandedShoe;
 
+// brand: "Nike"
+// colorway: "Black/Off Noir-Solar Flare-White"
+// created_at: "2020-07-26T18:22:34.665Z"
+// demographic: "men"
+// id: 693
+// image_url: "https://stockx.imgix.net/Nike-Zoom-Freak-2-Black-White.png?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1594978755"
+// model: "Nike Zoom Freak 2"
+// product: "Black White"
+// release_date: "2020-08-07T23:59:59.000Z"
+// retail_price: 130
+// small_image_url: "https://stockx.imgix.net/Nike-Zoom-Freak-2-Black-White.png?fit=fill&bg=FFFFFF&w=300&h=214&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1594978755"
+// thumb_url: "https://stockx.imgix.net/Nike-Zoom-Freak-2-Black-White.png?fit=fill&bg=FFFFFF&w=140&h=100&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1594978755"
+// title: "Nike Zoom Freak 2 Black White"
+// updated_at: "2020-07-26T18:22:34.665Z"
+// year: 2020
--- src/Shoes/Shoes.js
+++ src/Shoes/Shoes.js
@@ -3,11 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Shoes = ({...props }) => {
     return (
-        <section>
-            <h1>Model: {props.model}</h1>
-            <p>Release Date: {props.release_date}</p>
-            <p>Color: {props.colorway}</p>
-            <p>Brand: {props.brand}</p>
+        <section> 
             <p>Cost: {props.retail_price}</p>
             <NavLink to={`/shoe/${props.id}`}>
                 <button type='button'>
