1be42b9876d85c3090d09661d913c589e9d2af97
Jeremiah Black <jeremiahblackol@gmail.com>
2020-07-28 08:44:35 -0600

    played with styling

--- src/App/App.js
+++ src/App/App.js
@@ -24,9 +24,23 @@ class App extends Component {
     }
   }
 
+  // calculate range
+  // at this value on the range
+  // a certain shoe will display
+  // we can have entries
+  // and sort them by price
+  // at index 0 we will have the opportunity to give to the
+  // charity of your choice
+  // however much you choose
+  // 
+  // but once you get past the 0, historical shoes shoes appear
+  // and their prices are evaluated through time
+  // it will represent a kind of encyclopedia
+  // on shoes as we know them.
+
   // post new shoe
 
-  addShoeToDatabase = ()
+  // addShoeToDatabase = ()
     
   render() {
     // console.log(this.state)
@@ -35,16 +49,31 @@ class App extends Component {
         <section className="header">
           <h1>Sole Searchin'</h1>
           <p>Find your sole mate</p>
-          <NavLink to={"/submitShoe"}>
-            <button className="submitButton">
-              Add Shoe
-            </button>
-          </NavLink>
-          <NavLink to={"/"}>
-            <button className="submitButton">
-              Home
-            </button>
-          </NavLink>
+          <section className="headerButtonsContainer">
+            <NavLink to={"/submitShoe"}>
+              <button className="submitButton">
+                Add Sneaker
+              </button>
+            </NavLink>
+            <NavLink to={"/"}>
+              <button className="submitButton">
+                Home
+              </button>
+            </NavLink>
+            <NavLink to={"theForum"}>
+              <button className="submitButton">
+                The Forum
+              </button>
+            </NavLink><NavLink to={"codeChallenges"}>
+              <button className="submitButton">
+                codeChallenges
+              </button>
+            </NavLink><NavLink to={"/"}>
+              <button className="submitButton">
+                Networking
+              </button>
+            </NavLink>
+          </section>
         </section>
         <section className="main-body" data-testid="shoe-area">
           <Route exact path="/" render = { () => <ShoeBox shoes={this.state.shoes}/>} />
